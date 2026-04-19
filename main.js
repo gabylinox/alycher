eb8e73b84205069f40b984fe299f73a3e11af669
Gabriela Oliveira Lino <ol.gabriela06@gmail.com>
2026-04-19 12:35:36 -0300

    primeiro comit

---
+++ main.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+    // Aguarda um pequeno delay para iniciar a animação de crescimento
+    setTimeout(() => {
+        document.body.classList.remove("not-loaded");
+    }, 500);
+};
